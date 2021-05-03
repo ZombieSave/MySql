fe9a86279694edd7b561ff8791e941c391adc989
mars <you@example.com>
2021-05-03 16:43:06 +0300

    ДЗ к уроку 6

---
+++ Урок 6/Задача3.sql
@@ -0,0 +1,11 @@
+﻿-- Определить кто больше поставил лайков (всего) - мужчины или женщины?
+
+SELECT IF ((SELECT p.gender FROM profiles p WHERE p.user_id = l.user_id) = 'M', 'Мужчины', 'Женщины') gender_like, COUNT(*) like_count FROM likes l
+GROUP BY gender_like
+ORDER BY like_count DESC
+LIMIT 1
+
+
+
+
+
---
+++ Урок 6/Задача4.sql
@@ -0,0 +1,10 @@
+﻿-- Вывести для каждого пользователя количество созданных сообщений, постов, загруженных медиафайлов и поставленных лайков.
+
+SELECT CONCAT(u.last_name, ' ', u.first_name) user_name,
+(SELECT COUNT(*) FROM messages m WHERE m.from_user_id = u.id) number_of_messages,
+(SELECT COUNT(*) FROM posts p WHERE p.user_id = u.id) number_of_posts,
+(SELECT COUNT(*) FROM media m WHERE m.user_id = u.id) number_of_mediafiles,
+(SELECT COUNT(*) FROM likes l WHERE l.user_id = u.id) number_of_likes
+FROM users u;
+
+
