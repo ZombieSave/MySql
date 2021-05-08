422053e858356c2096e75b41d944b77a71499dc9
mars <you@example.com>
2021-05-08 20:14:10 +0300

    1

--- Урок 7/Задача1.sql
+++ Урок 7/Задача1.sql
@@ -1,4 +1,10 @@
 ﻿-- Составьте список пользователей users, которые осуществили хотя бы один заказ orders в интернет магазине.
 
+-- a)
+SELECT * FROM users u 
+WHERE u.id IN (SELECT u.id FROM users u
+               JOIN orders o ON o.user_id = u.id);
+
+-- b)
 SELECT * FROM users u
 WHERE EXISTS (SELECT 1 FROM orders o WHERE o.user_id = u.id);
