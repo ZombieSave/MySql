e674b6aa097deeeb197b1e4199a0c8d7f9074196
mars <you@example.com>
2021-05-14 22:49:48 +0300

    ДЗ к уроку 9

---
+++ Урок 9/Транзакции, переменные, представления.Задача2.sql
@@ -0,0 +1,8 @@
+﻿-- Создайте представление, которое выводит название name товарной позиции из таблицы products и соответствующее название каталога name из таблицы catalogs.
+
+USE shop
+
+CREATE OR REPLACE VIEW v_prod_cat
+AS
+SELECT p.name product_name, c.name catalog_name FROM products p
+JOIN catalogs c ON c.id = p.catalog_id;
---
+++ Урок 9/Транзакции, переменные, представления.Задача4.sql
@@ -0,0 +1,20 @@
+﻿-- (по желанию) Пусть имеется любая таблица с календарным полем created_at. Создайте запрос, который удаляет устаревшие записи из таблицы, оставляя только 5 самых свежих записей.
+
+USE shop
+-- видимо, нужно применить что-нибудь из темы урока?
+-- как вариант
+
+DROP TABLE IF EXISTS t_order_ids;
+
+CREATE TEMPORARY TABLE t_order_ids
+(
+  id bigint
+);
+
+INSERT INTO t_order_ids
+SELECT o.id FROM orders o
+ORDER BY o.created_at DESC
+LIMIT 5;
+
+DELETE FROM orders 
+WHERE id NOT IN (SELECT id FROM t_order_ids);
