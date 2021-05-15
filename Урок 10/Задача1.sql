4dcf9b7554e2c31571da0776127b0d28a938b768
mars <you@example.com>
2021-05-15 15:57:29 +0300

    ДЗ к уроку 10

---
+++ Урок 10/Задача1.sql
@@ -0,0 +1,21 @@
+﻿-- Проанализировать какие запросы могут выполняться наиболее часто в процессе работы приложения и добавить необходимые индексы.
+CREATE UNIQUE INDEX communities_name_uq ON communities(name); 
+CREATE UNIQUE INDEX friendship_statuses_name_uq ON friendship_statuses(name); 
+CREATE UNIQUE INDEX media_types_name_uq ON media_types(name); 
+
+CREATE INDEX communities_users_community_id_ix ON communities_users (community_id);
+CREATE INDEX communities_users_user_id_ix ON communities_users (user_id);
+
+CREATE INDEX users_first_name_last_name_ix ON users (first_name, last_name);
+
+CREATE INDEX users_first_name_last_name_ix ON users (first_name, last_name);
+
+CREATE INDEX friendship_user_id_friend_id_ix ON friendship (user_id, friend_id);
+
+CREATE INDEX media_user_id_filename_ix ON media (user_id, filename);
+
+CREATE INDEX messages_from_user_to_user_ix ON messages (from_user_id, to_user_id);
+
+CREATE INDEX posts_user_id_community_id_ix ON posts (user_id, community_id);
+
+
---
+++ Урок 10/Задача2.sql
@@ -0,0 +1,33 @@
+﻿/*
+Задание на оконные функции
+Построить запрос, который будет выводить следующие столбцы:
+имя группы
+среднее количество пользователей в группах (сумма количестива пользователей во всех группах делённая на количество групп)
+самый молодой пользователь в группе (желательно вывести имя и фамилию)
+самый старший пользователь в группе (желательно вывести имя и фамилию)
+количество пользователей в группе
+всего пользователей в системе (количество пользователей в таблице users)
+отношение в процентах для последних двух значений (общее количество пользователей в группе / всего пользователей в системе) * 100
+*/
+
+
+SELECT res.group_name 'группа',
+       SUM(res.users_in_groups) OVER() / res.total_communities 'среднее количество пользователей в группах',
+       res.youngest_man 'самый молодой пользователь в группе',
+       res.oldest_man 'самый старший пользователь в группе',
+       res.users_in_groups 'количество пользователей в группе',
+       res.total_users 'всего пользователей в системе',
+       (res.users_in_groups / res.total_users) * 100 'общее количество пользователей в группе / всего пользователей в системе) * 100' 
+                     FROM (SELECT c.name group_name,
+                             FIRST_VALUE(CONCAT(u.first_name, ' ', u.last_name)) OVER (PARTITION BY cu.community_id ORDER BY p.birthday DESC) youngest_man,
+                             FIRST_VALUE(CONCAT(u.first_name, ' ', u.last_name)) OVER (PARTITION BY cu.community_id ORDER BY p.birthday) oldest_man,
+                             COUNT(*) OVER (PARTITION BY cu.community_id) users_in_groups,
+                             (SELECT COUNT(*) FROM users) total_users,
+                             (SELECT COUNT(*) FROM communities) total_communities,
+                             ROW_NUMBER() OVER (PARTITION BY cu.community_id) rn
+                             FROM communities c
+                             JOIN communities_users cu ON cu.community_id = c.id
+                             JOIN profiles p ON p.user_id = cu.user_id
+                             JOIN users u ON u.id = p.user_id) res
+WHERE res.rn = 1;
+
