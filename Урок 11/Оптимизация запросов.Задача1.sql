0602fe88ecfe67792e4324f738c9c6d39036a620
mars <you@example.com>
2021-05-22 16:55:47 +0300

    ДЗ к уроку 11

---
+++ Урок 11/Оптимизация запросов.Задача1.sql
@@ -0,0 +1,41 @@
+﻿USE shop;
+
+DROP TABLE IF EXISTS logs;
+CREATE TABLE logs
+(
+table_name varchar(64),
+id BIGINT UNSIGNED,
+name VARCHAR(255),
+created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+) ENGINE=ARCHIVE; 
+
+
+DROP TRIGGER IF EXISTS t_products_arch;
+CREATE TRIGGER t_products_arch AFTER INSERT ON products
+  FOR EACH ROW
+BEGIN
+  INSERT INTO logs
+  (table_name, id, name)
+  VALUES('products', NEW.id, NEW.name);
+END;
+
+DROP TRIGGER IF EXISTS t_catalogs_arch;
+CREATE TRIGGER t_catalogs_arch AFTER INSERT ON catalogs
+  FOR EACH ROW
+BEGIN
+  INSERT INTO logs
+  (table_name, id, name)
+  VALUES('catalogs', NEW.id, NEW.name);
+END;
+
+DROP TRIGGER IF EXISTS t_users_arch;
+CREATE TRIGGER t_users_arch AFTER INSERT ON users
+  FOR EACH ROW
+BEGIN
+  INSERT INTO logs
+  (table_name, id, name)
+  VALUES('users', NEW.id, NEW.name);
+END;
+
+
+
